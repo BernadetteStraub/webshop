27779e91eb0c02745ea19c60d0016b008e9bddd4
Bernadette Straub <str.bernadette@gmail.com>
2025-07-28 21:52:34 +0200

    url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://webshop-bernadettes-projects-a7d6ae00.vercel.app'  // Replace with your local API URL
+  apiUrl: 'https://webshop-vert-three.vercel.app'
 };
